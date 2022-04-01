09602a5b2ae637720315e14eb75839420bf15e8f
osclancer <osclancer@gmail.com>
2022-04-02 01:19:28 +0200

    Add login test

--- src/__tests__/session.test.ts
+++ src/__tests__/session.test.ts
@@ -1,8 +1,35 @@
+import request from 'supertest';
+import server from '../server';
+import { signUp } from './helpers';
+
+const app = server();
+
 describe('session', () => {
 	describe('create session', () => {
 		describe('given an invalid credentials', () => {
-			it('should return 403 unAuthenticated', () => {
-				expect(true).toBe(true);
+			it('should return 401 unAuthenticated', () => {
+				request(app)
+					.post('/api/sessions')
+					.send({ email: 'fakemail@gmail.com', password: 123456 })
+					.expect(401);
+			});
+		});
+
+		describe('given valid credentials', () => {
+			it('should return accessToken && refreshToken', async () => {
+				await signUp();
+
+				request(app)
+					.post('/api/sessions')
+					.send({
+						email: 'muhammed@gmail.com',
+						password: '123456',
+					})
+					.then((data) => {
+						expect(data).toHaveProperty('accessToken');
+						expect(data).toHaveProperty('refreshToken');
+					})
+					expect(200);
 			});
 		});
 	});
